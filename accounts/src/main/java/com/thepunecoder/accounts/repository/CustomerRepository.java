099c2e4a2158c1f242e8c13c04071d5f536ef2f5
moto60954 <moto60954@gmail.com>
2025-09-06 21:21:26 +0530

    Initial commit

---
+++ accounts/src/main/java/com/thepunecoder/accounts/repository/CustomerRepository.java
@@ -0,0 +1,10 @@
+package com.thepunecoder.accounts.repository;
+
+import com.thepunecoder.accounts.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+}
