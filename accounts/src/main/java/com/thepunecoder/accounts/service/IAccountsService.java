1ffff9f3cdc8a8f436759ea478160d572de4b943
thepunecoder <moto60954@gmail.com>
2025-11-26 15:07:13 +0530

    READ API inside accounts microservice

--- accounts/src/main/java/com/thepunecoder/accounts/service/IAccountsService.java
+++ accounts/src/main/java/com/thepunecoder/accounts/service/IAccountsService.java
@@ -5,4 +5,6 @@ import com.thepunecoder.accounts.dto.CustomerDto;
 public interface IAccountsService {
 
     void createAccount(CustomerDto customerDto);
-    }   
+
+    CustomerDto fetchAccount(String mobileNumber);
+}   
